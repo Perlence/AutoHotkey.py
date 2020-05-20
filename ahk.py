b66617f1e4850b42613535e9a8314eeaa139be76
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2020-05-20 22:31:11 +0300

    Add more TODOs in ahk.py

--- ahk.py
+++ ahk.py
@@ -4,6 +4,9 @@ from functools import partial
 import _ahk  # noqa
 
 
+# TODO: Write an __all__.
+
+
 class AHKError(Exception):
     pass
 
@@ -18,16 +21,22 @@ def hotkey(key_name, func=None, buffer=None, priority=0, max_threads=None,
         return partial(hotkey, key_name, buffer=buffer, priority=priority,
                        max_threads=max_threads, input_level=input_level)
 
+    # TODO: Handle case when func == "AltTab" or other substitutes.
     _ahk.set_callback(f"Hotkey {key_name}", func)
     # TODO: Set the options.
     # TODO: Change options of the existing hotkeys.
     _ahk.call_cmd("Hotkey", key_name, "HotkeyLabel")
 
 
+def remap_key(origin_key, destination_key):
+    # TODO: Implement key remapping, e.g. Esc::CapsLock.
+    raise NotImplementedError()
+
+
 @contextmanager
 def hotkey_context():
     # TODO: Implement `Hotkey, If` commands.
-    ...
+    raise NotImplementedError()
 
 
 def send(keys):
