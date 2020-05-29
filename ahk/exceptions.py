55195e1f6bfdfc85da7b1affd95df87786b71e2a
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2020-05-29 22:28:37 +0300

    Add extended Error class

---
+++ ahk/exceptions.py
@@ -0,0 +1,9 @@
+class Error(Exception):
+    def __init__(self, message, what=None, extra=None, file=None, line=None):
+        super().__init__(message)
+        # TODO: Add AHK exception info to Python traceback?
+        self.message = message
+        self.what = what
+        self.extra = extra
+        self.file = file
+        self.line = line
