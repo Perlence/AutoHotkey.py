f18abfc1e42411502796697f925d27a8029c9656
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2020-06-23 20:10:54 +0300

    Make the batch file more universal

--- EmbedPython.bat
+++ EmbedPython.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\AutoHotkey\AutoHotkey.exe" "c:\Users\Sviatoslav\Workspace\EmbedPython\EmbedPython.ahk" %* 2>&1 | wsl cat
+"C:\Program Files\AutoHotkey\AutoHotkey.exe" "%~dn0.ahk" %* 2>&1 | wsl cat
