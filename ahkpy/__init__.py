4a9a10afe14974c9b2ac20ce2c3393b66ded2e77
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2020-07-09 23:05:02 +0300

    Read version from __init__.py

--- ahkpy/__init__.py
+++ ahkpy/__init__.py
@@ -22,3 +22,5 @@ from .flow import *  # noqa
 from .gui import *  # noqa
 from .keys import *  # noqa
 from .window import *  # noqa
+
+__version__ = "0.1a0"
--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+import re
 from setuptools import setup
+
+with open("ahkpy/__init__.py", encoding="utf-8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 setup(
     name='AutoHotkey.py',
-    version='0.1a0',
+    version=version,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
