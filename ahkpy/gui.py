4540375a27391d6c4710a5417a0dc5bfeab8e1a7
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2020-07-30 23:54:20 +0300

    Implement ToolTip

--- ahkpy/gui.py
+++ ahkpy/gui.py
@@ -1,13 +1,27 @@
-from dataclasses import dataclass
+import dataclasses as dc
+import enum
+import threading
+from collections import deque
 from typing import Callable
 
 import _ahk  # noqa
 
 __all__ = [
+    "CoordMode",
     "MessageHandler",
+    "ToolTip",
     "message_box",
     "on_message",
 ]
+
+
+class CoordMode(enum.Enum):
+    SCREEN = 'screen'
+    WINDOW = 'window'
+    CLIENT = 'client'
+
+
+coord_mode_lock = threading.RLock()
 
 
 def message_box(text=None, title="", options=0, timeout=None):
@@ -35,7 +49,7 @@ def on_message(msg_number, func=None, *, max_threads=1, prepend_handler=False):
     return on_message_decorator(func)
 
 
-@dataclass(frozen=True)
+@dc.dataclass(frozen=True)
 class MessageHandler:
     msg_number: int
     func: Callable
@@ -46,6 +60,60 @@ class MessageHandler:
         _ahk.call("OnMessage", self.msg_number, self.func, 0)
 
 
-def tooltip():
-    # TODO: Implement tooltips.
-    raise NotImplementedError()
+@dc.dataclass
+class ToolTip:
+    text: str = None
+    x: int = None
+    y: int = None
+    coord_mode: CoordMode = CoordMode.SCREEN
+    _id: int = dc.field(default=None, repr=False)
+
+    _pool_lock = threading.RLock()
+    _pool = deque(range(1, 21))
+
+    def show(self, text=None, x=None, y=None, coord_mode=None):
+        # TODO: Write tests.
+
+        if not text and not self.text:
+            raise ValueError("text must not be empty")
+        elif text:
+            self.text = text
+
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
+        x = self.x if self.x is not None else ""
+        y = self.y if self.y is not None else ""
+
+        if coord_mode is not None:
+            if isinstance(coord_mode, str):
+                coord_mode = CoordMode(coord_mode.lower())
+            self.coord_mode = coord_mode
+
+        tooltip_id = self._acquire()
+        with coord_mode_lock:
+            _ahk.call("CoordMode", "ToolTip", self.coord_mode.value)
+            _ahk.call("ToolTip", str(self.text), x, y, tooltip_id)
+
+    def hide(self):
+        if self._id is None:
+            return
+        _ahk.call("ToolTip", "", "", "", self._id)
+        self._release()
+
+    def _acquire(self):
+        if self._id is not None:
+            return self._id
+        try:
+            with ToolTip._pool_lock:
+                self._id = ToolTip._pool.popleft()
+            return self._id
+        except IndexError:
+            raise RuntimeError("cannot show more than 20 tooltips") from None
+
+    def _release(self):
+        with ToolTip._pool_lock:
+            if self._id not in ToolTip._pool:
+                ToolTip._pool.appendleft(self._id)
+            self._id = None
