d8a88bde72c88184b35203a3735034a2f3b216ef
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2020-10-20 11:42:36 +0300

    Add onenote_ctx example

---
+++ examples/onenote_ctx.py
@@ -0,0 +1,12 @@
+from functools import partial
+
+import ahkpy as ahk
+
+
+onenote_ctx = ahk.windows.active_window_context(title="- OneNote for Windows 10$", match="regex")
+# Send em dash when typing double dashes
+onenote_ctx.hotstring("--", "—", wait_for_end_char=False)
+# Add line before
+onenote_ctx.hotkey("^+Enter", partial(ahk.send, "{Home}{Enter}{Up}"))
+# Add line after
+onenote_ctx.hotkey("^Enter", partial(ahk.send, "{End}{Enter}"))
