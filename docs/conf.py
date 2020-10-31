c12a213bc0b868b268276dcad67bdc822fb69326
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2020-10-31 23:27:52 +0300

    Docs: Remove extra line

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
-#
 html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
