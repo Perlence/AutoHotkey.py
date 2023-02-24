41eb23248e7773e824f887e93861ed0c2235ed02
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2023-02-24 21:44:30 +0300

    Switch to the src/ layout
    The previous layout messed with running the CLI tests in tox. Instead of
    importing ahkpy from the tox environment, Python imported ahkpy from the
    working directory.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import re
 from setuptools import setup
 
-with open("ahkpy/__init__.py", encoding="utf-8") as f:
+with open("src/ahkpy/__init__.py", encoding="utf-8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 setup(
